451e05be600762c3e612fb9a97b1daae6b94858d
mrtsway <wscfx0817@gmail.com>
2022-08-06 19:05:24 +0800

    Add l flag to count lines instead, and test it

--- main_test.go
+++ main_test.go
@@ -9,7 +9,18 @@ func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word word word word\n")
 	exp := 4
 
-	res := count(b)
+	res := count(b, false)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead\n", exp, res)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word word word word\nline\nline")
+	exp := 3
+
+	res := count(b, true)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead\n", exp, res)
