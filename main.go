061a8b1af8153d9fb1fa4ad170bb7347e3140cef
mrtsway <wscfx0817@gmail.com>
2022-10-01 11:42:03 +0800

    Add reading data from files

--- main.go
+++ main.go
@@ -11,8 +11,21 @@ import (
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
 	bytes := flag.Bool("b", false, "Count bytes")
+	files := flag.Bool("f", false, "Read data from files")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines, *bytes))
+
+	if *files {
+		for i, filename := range flag.Args() {
+			file, err := os.Open(filename)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			fmt.Printf("%d. %s: %d\n", i+1, filename, count(file, *lines, *bytes))
+		}
+	} else {
+		fmt.Println(count(os.Stdin, *lines, *bytes))
+	}
 }
 
 func count(r io.Reader, countLines bool, countBytes bool) int {
--- main_test.go
+++ main_test.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 	"testing"
 )
 
@@ -35,5 +40,48 @@ func TestCountBytes(t *testing.T) {
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead\n", exp, res)
+	}
+}
+
+var (
+	binName = "wordcounter"
+	files   = []string{"file1", "file2", "file3"}
+)
+
+func TestMain(m *testing.M) {
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+
+	fmt.Println("Building tool...")
+	build := exec.Command("go", "build", "-o", binName)
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running tests...")
+	result := m.Run()
+	fmt.Println("Cleanning up...")
+	os.Remove(binName)
+
+	os.Exit(result)
+}
+
+func TestCountWordsFromFiles(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cmdPath := filepath.Join(dir, binName)
+	args := append([]string{"-f"}, files...)
+	cmd := exec.Command(cmdPath, args...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := "1. file1: 5\n2. file2: 7\n3. file3: 6\n"
+	if string(out) != expected {
+		t.Errorf("Expected %q, got %q instead\n", expected, string(out))
 	}
 }
